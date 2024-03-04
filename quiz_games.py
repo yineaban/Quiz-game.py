03d11268b0a91232635a865e55d824cb547269a3
yinager kinde <121093513+yineaban@users.noreply.github.com>
2024-03-04 05:01:12 -0800

    Electrical and computer engineering
    Electrical and computer Engineering
    studied in addis ababa science and technology university

--- quiz_games.py
+++ quiz_games.py
@@ -3,9 +3,11 @@ play=input("do you want to play? ")
 if play!="yes":
     quit()
 print("let us play")
+score=0
 answer=input("what cpu stands for? ")
 if answer=="central processing unit":
     print("correct")
+    score+=1
     
 else:
     print("Incorrect")
@@ -13,18 +15,25 @@ else:
 answer=input("what psu stands for? ")
 if answer=="power supply unit":
     print("correct")
+    score+=1
+    
     
 else:
     print("Incorrect")
 answer=input("what RAM stands for? ")
 if answer=="random access memory":
     print("correct")
+    score+=1
+    
     
 else:
     print("Incorrect")
 answer=input("what Rom stands for? ")
 if answer=="read only memory":
     print("correct")
+    score+=1
+    
     
 else:
-    print("Incorrect"
+    print("Incorrect")
+print("you have got total score of",score)
