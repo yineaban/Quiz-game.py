b73c61525bfdcb17202e919ccb4f30d2c0b07c3e
yinager kinde <121093513+yineaban@users.noreply.github.com>
2024-03-04 04:27:30 -0800

    Create quiz_games.py
    quize_game as beginner

---
+++ quiz_games.py
@@ -0,0 +1,30 @@
+print("well come to my computer quiz")
+play=input("do you want to play? ")
+if play!="yes":
+    quit()
+print("let us play")
+answer=input("what cpu stands for? ")
+if answer=="central processing unit":
+    print("correct")
+    
+else:
+    print("Incorrect")
+    
+answer=input("what psu stands for? ")
+if answer=="power supply unit":
+    print("correct")
+    
+else:
+    print("Incorrect")
+answer=input("what RAM stands for? ")
+if answer=="random access memory":
+    print("correct")
+    
+else:
+    print("Incorrect")
+answer=input("what Rom stands for? ")
+if answer=="read only memory":
+    print("correct")
+    
+else:
+    print("Incorrect"
